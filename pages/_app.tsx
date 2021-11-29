53c0462c08540c7fd4bf27fd312cca544be61424
Sergey Mikheev <mikheevs11@gmail.com>
2021-11-29 23:36:36 +0300

    Added axios + react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,18 @@ import React from 'react'
 import type { AppProps } from 'next/app'
 import Header from '../src/components/Header/Header';
 
+// Assets
 import 'semantic-ui-css/semantic.min.css';
 import '../styles/globals.css'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
 	return (
-		<div>
+		<>
 			<Header />
-			<Component {...pageProps} />
-		</div>
+			<div className='page-content'>
+				<Component {...pageProps} />
+			</div>
+		</>
 	)
 }
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import Link from 'next/link';
-import Image from 'next/image';
 import { Logo } from '../uiKit/Logo';
 import { RouteLink } from '../uiKit/RouteLink';
 
